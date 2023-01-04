89fc9358a42ea176470ce5e90282baa69d075ac4
Jonatas Rodolfo Pereira dos Santos <rodolfostark@ufrn.edu.br>
2023-01-03 21:39:08 -0300

    chore: uploading the project to the remote repo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { theme } from '../styles/theme';
+
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { makeServer } from '../services/mirage';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from '../services/queryClient';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp
